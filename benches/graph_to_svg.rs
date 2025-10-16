391bccc2e785200c0a75a82ba7eaea63c1da21da
Raoul Luqué <raoulsluque@gmail.com>
2025-10-16 19:00:58 +0200

    perf: Setup basic benchmark and create buffer with min capacity for graph_to_svg

---
+++ benches/graph_to_svg.rs
@@ -0,0 +1,25 @@
+mod common;
+use common::build_2d_grid;
+
+use criterion::{Criterion, criterion_group, criterion_main};
+use visgraph;
+
+fn graph_to_svg_benchmark(c: &mut Criterion) {
+    let graph = build_2d_grid(100, 100);
+
+    c.bench_function("graph_to_svg", |b| {
+        b.iter(|| {
+            visgraph::graph_to_svg(
+                &graph,
+                |node_id| {
+                    let (x, y) = *graph.node_weight(node_id).unwrap();
+                    (x as f32, y as f32)
+                },
+                |node_id| format!("{:?}", graph.node_weight(node_id).unwrap()),
+            )
+        })
+    });
+}
+
+criterion_group!(benches, graph_to_svg_benchmark);
+criterion_main!(benches);
--- src/graph_to_svg.rs
+++ src/graph_to_svg.rs
@@ -1,26 +1,25 @@
-use petgraph::visit::{EdgeRef, IntoEdgeReferences, IntoNodeReferences, NodeRef};
+use petgraph::visit::{
+    EdgeIndexable, EdgeRef, IntoEdgeReferences, IntoNodeReferences, NodeIndexable, NodeRef,
+};
 
 const RADIUS: f32 = 20.0;
 const FONT_SIZE: f32 = 14.0;
 
 pub fn graph_to_svg<G, FnPos, FnLabel>(graph: G, position_map: FnPos, label_map: FnLabel) -> String
 where
-    G: IntoNodeReferences + IntoEdgeReferences,
+    G: IntoNodeReferences + IntoEdgeReferences + NodeIndexable + EdgeIndexable,
     FnPos: Fn(G::NodeId) -> (f32, f32),
     FnLabel: Fn(G::NodeId) -> String,
 {
-    let mut svg_data = String::new();
-    svg_data.push_str(r#"<svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">"#);
+    let mut svg_buffer = String::with_capacity(graph.node_bound() * 120 + graph.edge_bound() * 50);
+    svg_buffer.push_str(r#"<svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">"#);
 
     // Add nodes
     for node in graph.node_references() {
         let id = node.id();
         let (coord_x, coord_y) = position_map(id);
         let node_label = label_map(id);
-        svg_data.push_str(&format!(
-            "<circle cx=\"{coord_x}\" cy=\"{coord_y}\" r=\"{RADIUS}\" fill=\"white\" stroke=\"black\"/>\n
-            <text x=\"{coord_x}\" y=\"{coord_y}\" font-size=\"{FONT_SIZE}\" font-family='Arial, sans-serif' fill=\"black\" text-anchor=\"middle\" dominant-baseline=\"central\">{node_label}</text>\n",
-        ));
+        draw_node(&mut svg_buffer, coord_x, coord_y, &node_label);
     }
 
     // Add edges
@@ -29,15 +28,38 @@ where
         let target = edge.target();
         let (coord_x_source, coord_y_source) = position_map(source);
         let (coord_x_target, coord_y_target) = position_map(target);
-        svg_data.push_str(&format!(
-            "<line x1=\"{}\" y1=\"{}\" x2=\"{}\" y2=\"{}\" stroke=\"black\"/>\n",
-            coord_x_source + RADIUS,
+        draw_edge(
+            &mut svg_buffer,
+            coord_x_source,
             coord_y_source,
-            coord_x_target - RADIUS,
-            coord_y_target
-        ));
+            coord_x_target,
+            coord_y_target,
+        );
     }
 
-    svg_data.push_str("</svg>");
-    svg_data
+    svg_buffer.push_str("</svg>");
+    svg_buffer
+}
+
+fn draw_node(svg_buffer: &mut String, coord_x: f32, coord_y: f32, node_label: &str) {
+    svg_buffer.push_str(&format!(
+            "   <circle cx=\"{coord_x}\" cy=\"{coord_y}\" r=\"{RADIUS}\" fill=\"white\" stroke=\"black\"/>\n
+    <text x=\"{coord_x}\" y=\"{coord_y}\" font-size=\"{FONT_SIZE}\" font-family='Arial, sans-serif' fill=\"black\" text-anchor=\"middle\" dominant-baseline=\"central\">{node_label}</text>\n",
+        ));
+}
+
+fn draw_edge(
+    svg_buffer: &mut String,
+    coord_x_source: f32,
+    coord_y_source: f32,
+    coord_x_target: f32,
+    coord_y_target: f32,
+) {
+    svg_buffer.push_str(&format!(
+        "<line x1=\"{}\" y1=\"{}\" x2=\"{}\" y2=\"{}\" stroke=\"black\"/>\n",
+        coord_x_source + RADIUS,
+        coord_y_source,
+        coord_x_target - RADIUS,
+        coord_y_target
+    ));
 }
