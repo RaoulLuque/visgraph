27abf232c675cf711376d07c6dd9749332f66edc
Raoul Luqué <raoulsluque@gmail.com>
2025-10-21 21:29:02 +0200

    perf: Fix benchmarks

--- benches/graph_to_svg.rs
+++ benches/graph_to_svg.rs
@@ -9,14 +9,13 @@ fn graph_to_svg_benchmark(c: &mut Criterion) {
 
     c.bench_function("graph_to_svg", |b| {
         b.iter(|| {
-            visgraph::graph_to_svg_with_positions(
+            visgraph::graph_to_svg::graph_to_svg_with_positions(
                 &graph,
                 |node_id| {
                     let (x, y) = *graph.node_weight(node_id).unwrap();
                     (x as f32, y as f32)
                 },
-                |node_id| format!("{:?}", graph.node_weight(node_id).unwrap()),
-                visgraph::settings::Settings::default(),
+                &visgraph::settings::Settings::default(),
             )
         })
     });
