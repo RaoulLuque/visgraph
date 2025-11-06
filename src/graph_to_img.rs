444922c760a1b6dd1c2b33a442ab6353ae30bfd9
Raoul Luqué <raoulsluque@gmail.com>
2025-11-06 21:18:22 +0100

    refactor: Remove svg_to_img from default features and move into separate module/file

---
+++ src/graph_to_img.rs
@@ -0,0 +1,58 @@
+//! Functionality to convert graphs to PNGs.
+//!
+//! The main function is [`graph_to_img`] which generates a PNG from a graph using either a
+//! custom position map or a predefined layout algorithm, respectively.
+//!
+//! Note that if a position map is used, it should return normalized coordinates between 0.0 and
+//! 1.0.
+//!
+//! For examples, see the `examples/` directory.
+
+use petgraph::visit::{
+    EdgeIndexable, IntoEdgeReferences, IntoNeighborsDirected, IntoNodeReferences, NodeIndexable,
+};
+
+use crate::{
+    errors::VisGraphError, graph_to_svg::graph_to_svg_string, settings::Settings,
+    svg_to_img::svg_to_img,
+};
+
+/// Generate and save an image of a graph to the specified path.
+///
+/// # Settings
+///
+/// To configure the graph rendering, use the [`SettingsBuilder`](crate::settings::SettingsBuilder)
+/// struct.
+///
+/// # Usage
+///
+/// The following is an example taken from
+/// [`examples/default_settings.rs`](https://github.com/RaoulLuque/visgraph/blob/main/examples/default_settings.rs):
+/// ```
+#[allow(clippy::needless_doctest_main)]
+#[doc = include_str!("../examples/default_settings.rs")]
+/// ```
+/// More examples can be found in the [`examples`](https://github.com/RaoulLuque/visgraph/tree/main/examples)
+/// directory.
+#[cfg(feature = "svg_to_img")]
+pub fn graph_to_img<G, PositionMapFn, NodeLabelFn, EdgeLabelFn, NodeColoringFn, EdgeColoringFn>(
+    graph: G,
+    settings: &Settings<PositionMapFn, NodeLabelFn, EdgeLabelFn, NodeColoringFn, EdgeColoringFn>,
+    path: impl AsRef<std::path::Path>,
+) -> Result<(), VisGraphError>
+where
+    G: IntoNodeReferences
+        + IntoEdgeReferences
+        + NodeIndexable
+        + EdgeIndexable
+        + IntoNeighborsDirected,
+    PositionMapFn: Fn(G::NodeId) -> (f32, f32),
+    NodeLabelFn: Fn(G::NodeId) -> String,
+    EdgeLabelFn: Fn(G::EdgeId) -> String,
+    NodeColoringFn: Fn(G::NodeId) -> String,
+    EdgeColoringFn: Fn(G::EdgeId) -> String,
+{
+    let svg_data = graph_to_svg_string(graph, settings);
+    svg_to_img(&svg_data, settings.width, settings.height, path)?;
+    Ok(())
+}
--- src/lib.rs
+++ src/lib.rs
@@ -2,61 +2,18 @@
 #![warn(missing_debug_implementations, missing_docs)]
 
 mod errors;
+#[cfg(feature = "svg_to_img")]
+pub mod graph_to_img;
 pub mod graph_to_svg;
 pub mod layout;
 pub mod settings;
 #[cfg(feature = "svg_to_img")]
 pub mod svg_to_img;
 
+#[cfg(feature = "svg_to_img")]
+pub use graph_to_img::graph_to_img;
 pub use graph_to_svg::graph_to_svg;
 pub use layout::{hierarchical::Orientation, Layout};
-#[cfg(feature = "svg_to_img")]
-use petgraph::visit::IntoNeighborsDirected;
-use petgraph::visit::{EdgeIndexable, IntoEdgeReferences, IntoNodeReferences, NodeIndexable};
-#[cfg(feature = "svg_to_img")]
-use svg_to_img::svg_to_img;
-
-use crate::{errors::VisGraphError, graph_to_svg::graph_to_svg_string, settings::Settings};
-
-/// Generate and save an image of a graph to the specified path.
-///
-/// # Settings
-///
-/// To configure the graph rendering, use the [`SettingsBuilder`](crate::settings::SettingsBuilder)
-/// struct.
-///
-/// # Usage
-///
-/// The following is an example taken from
-/// [`examples/default_settings.rs`](https://github.com/RaoulLuque/visgraph/blob/main/examples/default_settings.rs):
-/// ```
-#[allow(clippy::needless_doctest_main)]
-#[doc = include_str!("../examples/default_settings.rs")]
-/// ```
-/// More examples can be found in the [`examples`](https://github.com/RaoulLuque/visgraph/tree/main/examples)
-/// directory.
-#[cfg(feature = "svg_to_img")]
-pub fn graph_to_img<G, PositionMapFn, NodeLabelFn, EdgeLabelFn, NodeColoringFn, EdgeColoringFn>(
-    graph: G,
-    settings: &Settings<PositionMapFn, NodeLabelFn, EdgeLabelFn, NodeColoringFn, EdgeColoringFn>,
-    path: impl AsRef<std::path::Path>,
-) -> Result<(), VisGraphError>
-where
-    G: IntoNodeReferences
-        + IntoEdgeReferences
-        + NodeIndexable
-        + EdgeIndexable
-        + IntoNeighborsDirected,
-    PositionMapFn: Fn(G::NodeId) -> (f32, f32),
-    NodeLabelFn: Fn(G::NodeId) -> String,
-    EdgeLabelFn: Fn(G::EdgeId) -> String,
-    NodeColoringFn: Fn(G::NodeId) -> String,
-    EdgeColoringFn: Fn(G::EdgeId) -> String,
-{
-    let svg_data = graph_to_svg_string(graph, settings);
-    svg_to_img(&svg_data, settings.width, settings.height, path)?;
-    Ok(())
-}
 
 #[cfg(test)]
 #[allow(missing_docs)]
